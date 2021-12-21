311332ad31bee32fc9d63c12d4bcaae4b31834f6
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-20 19:28:00 -0500

    Create upload_S3_Object_template.py

---
+++ upload_S3_Object_template.py
@@ -0,0 +1,17 @@
+import boto3
+import logging
+from botocore.exceptions import ClientError
+import os
+  
+  
+def upload_file(file_name, bucket, object_name=None):
+    
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
