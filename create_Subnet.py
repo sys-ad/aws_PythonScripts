fb4a88c242d75180b15f52a2580a8c3760d05207
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-28 22:09:24 -0500

    Update create_Subnet.py

--- create_Subnet.py
+++ create_Subnet.py
@@ -4,7 +4,7 @@
 # Example usage: For greater redundency and network traffic control, you may have several subnets in different AZs.
 import boto3
 ec2 = boto3.resource('ec2')
-vpc = ec2.Vpc('vpc-0aea582112524a3c0')                          # The Vpc's Id identifier
+vpc = ec2.Vpc('vpc-xxx..')       # The Vpc's Id identifier
 subnet = vpc.create_subnet(
     TagSpecifications=[
         {
@@ -18,8 +18,8 @@ subnet = vpc.create_subnet(
         },
     ],
     AvailabilityZone='us-east-1a',   # AZ ranges very by region
-    AvailabilityZoneId='use1-az1',    # AZ Id, varies. One way to check is through AWS CLI ----  $aws ec2 describe-availability-zones --region <your-region>
-    CidrBlock='10.11.0/16',
+    #AvailabilityZoneId='use1-az1',    # AZ Id, varies. One way to check is through AWS CLI ----  $aws ec2 describe-availability-zones --region <your-region>
+    CidrBlock='xxx.xxx.xxx.xxx/xx',    # Give the Subnet a CIDR block, should be within VPC CIDR
     DryRun=False,
     Ipv6Native=False
 ) 
