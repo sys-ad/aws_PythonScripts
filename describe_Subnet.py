d63a5b6cdce8638b0137bbc2d298b740cb04e476
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-29 23:28:27 -0500

    Update describe_Subnet.py

--- describe_Subnet.py
+++ describe_Subnet.py
@@ -4,7 +4,7 @@ import boto3
 
 client = boto3.client('ec2')
 response = client.describe_subnets(
-    SubnetIds=['subnet-id'],    # Enter valid subnet-id here. SubnetId is a list; may input more subnet-id(s).
+    SubnetIds=[input('Enter valid subnet-id: ')],    # Enter valid subnet-id here. SubnetId is a list; may input more subnet-id(s).
 )
 
 print(response)
