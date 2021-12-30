bb67cc43689b42743952d884bf333f90671bfca9
MoGo <95191519+sys-ad@users.noreply.github.com>
2021-12-29 22:50:44 -0500

    Update describe_VPC_Subnet.py

--- describe_VPC_Subnet.py
+++ describe_VPC_Subnet.py
@@ -2,7 +2,7 @@
 import boto3
 
 
-# Descibes specific VPC subnet. 
+# Descibes specific VPC subnet. Check the API Documentation for further granulation of the Filters list
 
 response = client.describe_subnets(
              Filters=[
