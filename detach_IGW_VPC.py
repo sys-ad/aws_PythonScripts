0d21a4a32c71f09751a526f1ff7d0a971ba99db3
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-04 23:52:49 -0500

    Update detach_IGW_VPC.py

--- detach_IGW_VPC.py
+++ detach_IGW_VPC.py
@@ -1,1 +1,8 @@
 # Detaches IGW to VPC
+import boto3
+
+ec2 = boto3.resource('ec2')
+vpc = ec2.Vpc('vpc-xxx..')    # VPC identifier here
+response = vpc.detach_internet_gateway(
+    InternetGatewayI='igw-xxx...'    # igw-id goes here
+)
