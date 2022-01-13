8fd7ea7ad9c8440ba3a43966b930eb03c32dd110
MoGo <95191519+sys-ad@users.noreply.github.com>
2022-01-12 21:35:55 -0500

    Update create_SG.py

--- create_SG.py
+++ create_SG.py
@@ -1,1 +1,24 @@
+#Python3.x
+
+# Creates security group
+
 import boto3
+client = boto3.client('ec2')
+response = client.create_security_group(
+    Description='Description',    # Description place holder
+    GroupName='xxx',    # Security group name
+    VpcId='xxx',    # Enter valid vpc-id
+    TagSpecifications=[
+        {
+             'ResourceType': 'security-group',
+             'Tags': [
+                 {
+                     'Key': 'Name',
+                     'Value': 'xxx'    # Key:Value pair tag; 
+                 },
+              ]
+        },
+     ]
+ )
+
+
